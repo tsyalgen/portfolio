02dcd52a089ac6cf0b2223f31fb75e07e38f7913
Aleksei Tsybin <tsyalgen@yandex.ru>
2021-07-29 00:15:53 +0300

    feature: complete skills and adaptive design for all current blocks

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,10 +1,73 @@
 import './Skills.css';
+import figma from '../../images/Figma.svg';
 
 const Skills = () => {
     return(
         <section id="skills" className="skills">
-            <h2 className="about__title">Skills</h2>
-            <p className="about__text">I work in such programs as</p>
+            <h2 className="skills__title">Skills</h2>
+            <p className="skills__text">I work in such programs as</p>
+            <ul className="skills__items">
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">HTML</p>
+                    </a>
+                </li>
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">CSS</p>
+                    </a>
+                </li>
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">JavaScript</p>
+                    </a>
+                </li>
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">ReactJS</p>
+                    </a>
+                </li>
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">Redux</p>
+                    </a>
+                </li>
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">NodeJS</p>
+                    </a>
+                </li>
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">NextJS</p>
+                    </a>
+                </li>
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">MongoDB</p>
+                    </a>
+                </li>
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">GIT</p>
+                    </a>
+                </li>
+                <li className="skills__item">
+                    <a className="skills__item__link" href="#" rel="noreferrer noopener" >
+                        <img src={figma} alt="figma logo" className="skills__item__image transparence"/>
+                        <p className="skills__item__name">Figma</p>
+                    </a>
+                </li>
+            </ul>
         </section>
     );
 }
