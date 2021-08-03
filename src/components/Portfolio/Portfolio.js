995edfb4c86a1710bd462ba156213c3aebd607da
Aleksei Tsybin <tsyalgen@yandex.ru>
2021-08-04 00:06:23 +0300

    feature: continue portfolio block

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -11,28 +11,20 @@ const Portfolio = () => {
         setIsChange(!isChange);
     }
 
-    // useEffect(() => {
-    //     // TODO пройтись по всем селекторам и потом замапить на все выбранные по листенеру? но лучше найти дургое решение
-    //     let item = document.querySelector('.portfolio__item')
-    //     item.addEventListener('mouseleave', () => {
-    //         setIsChange(false);
-    //         console.log(123);
-    //     });
-    // }, []);
-
     return (
         <section id="portfolio" className="portfolio">
             <h2 className="portfolio__title">Portfolio</h2>
             <ul className="portfolio__items">
                 <li className="portfolio__item">
                     {/*TODO вынести вот эту всю красоту в отедльный компонент?*/}
+                    {/*TODO СБОКУ ИНФО И ПРОСТО ПО НАВЕДЕНИЮ ПУСТЬ ВСПЛЫВАЕТ ВСЯ ИНФА!!!!!! БЕЗ ЧЕКБОКСА БИНГО НАХУЙ*/}
+                    {/*ТАМ ЖЕ СБОКУ ГИТХАБ И ЭКСТЕРНАЛ ЛИНК БОЖЕ, по наведению на инфо меняют цвет?*/}
                     <img src={job} alt="my job" className="portfolio__item__image"/>
                     <div className="portfolio__links">
                         <input className="portfolio__menu-toggle" id="menu-toggle"
                                type="checkbox" checked={isChange} onChange={() => {}}/>
                         <label className="portfolio__link__info" htmlFor="menu-toggle" onClick={handleToggle}/>
                         <div className="portfolio__menu-box">
-                            {/*TODO когда чекбокс чекед - это отображается на всех карточках портфолио!*/}
                             <h3>Movie Explorer</h3>
                             <h4>ваще тут будет красиво отредаченное описание все дела но пока так</h4>
                             <p>Дипломный проект Яндекс.Практикума.
