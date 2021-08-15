94705a8e230c2b7ee9b545052f4e8077beef889f
Aleksei Tsybin <tsyalgen@yandex.ru>
2021-08-15 04:24:01 +0300

    feature: portfolio component complete

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,82 +1,63 @@
 import './Portfolio.css';
 import job from '../../images/job.png';
-import {useEffect, useState} from "react";
 
 
 const Portfolio = () => {
-
-    const [isChange, setIsChange] = useState(false);
-
-    function handleToggle() {
-        setIsChange(!isChange);
-    }
 
     return (
         <section id="portfolio" className="portfolio">
             <h2 className="portfolio__title">Portfolio</h2>
             <ul className="portfolio__items">
                 <li className="portfolio__item">
-                    {/*TODO вынести вот эту всю красоту в отедльный компонент?*/}
-                    {/*TODO СБОКУ ИНФО И ПРОСТО ПО НАВЕДЕНИЮ ПУСТЬ ВСПЛЫВАЕТ ВСЯ ИНФА!!!!!! БЕЗ ЧЕКБОКСА БИНГО НАХУЙ*/}
-                    {/*ТАМ ЖЕ СБОКУ ГИТХАБ И ЭКСТЕРНАЛ ЛИНК БОЖЕ, по наведению на инфо меняют цвет?*/}
-                    <img src={job} alt="my job" className="portfolio__item__image"/>
+                    <img src={job} alt="my job" className="portfolio__item-image"/>
                     <div className="portfolio__links">
-                        <input className="portfolio__menu-toggle" id="menu-toggle"
-                               type="checkbox" checked={isChange} onChange={() => {}}/>
-                        <label className="portfolio__link__info" htmlFor="menu-toggle" onClick={handleToggle}/>
-                        <div className="portfolio__menu-box">
-                            <h3>Movie Explorer</h3>
-                            <h4>ваще тут будет красиво отредаченное описание все дела но пока так</h4>
-                            <p>Дипломный проект Яндекс.Практикума.
+                        <label className="portfolio__link-info"/>
+                        <div className="portfolio__item-info">
+                            <h3 className="portfolio__item-title">Movie Explorer</h3>
+                            <p className="portfolio__item-description">Дипломный проект Яндекс.Практикума.
                                 Представляет собой веб-приложение с функционалом поиска и
                                 сохранения в свой профиль фильмов с базы BeatMovie.</p>
-                            <p>Стек: React.js, Express.js, MongoDB.</p>
-                            <a href="https://github.com/tsyalgen/movies-explorer-frontend"
-                               target="_blank" rel="noreferrer noopener">тут будет иконка гитхаба</a>
+                            <p className="portfolio__item-description">Стек: React.js, Express.js, MongoDB.</p>
                         </div>
-                        <a className="portfolio__link__external" href="https://tsyalgen.nomoredomains.club/"
+                        <a className="portfolio__link-github" href="https://tsyalgen.nomoredomains.club/"
+                           target="_blank" rel="noreferrer noopener"/>
+                        <a className="portfolio__link-external" href="https://tsyalgen.nomoredomains.club/"
                            target="_blank" rel="noreferrer noopener"/>
                     </div>
                     Movie Explorer
                 </li>
                 <li className="portfolio__item">
-                    <img src={job} alt="my job" className="portfolio__item__image"/>
+                    <img src={job} alt="my job" className="portfolio__item-image"/>
                     <div className="portfolio__links">
-                        <input className="portfolio__menu-toggle" id="menu-toggle"
-                               type="checkbox" checked={isChange} onChange={() => {}}/>
-                        <label className="portfolio__link__info" htmlFor="menu-toggle" onClick={handleToggle}/>
-                        <div className="portfolio__menu-box">
-                            <h3>Movie Explorer</h3>
-                            <h4>ваще тут будет красиво отредаченное описание все дела но пока так</h4>
-                            <p>Дипломный проект Яндекс.Практикума.
+                        <label className="portfolio__link-info"/>
+                        <div className="portfolio__item-info">
+                            <h3 className="portfolio__item-title">Movie Explorer</h3>
+                            <p className="portfolio__item-description">Дипломный проект Яндекс.Практикума.
                                 Представляет собой веб-приложение с функционалом поиска и
                                 сохранения в свой профиль фильмов с базы BeatMovie.</p>
-                            <p>Стек: React.js, Express.js, MongoDB.</p>
-                            <a href="https://github.com/tsyalgen/movies-explorer-frontend"
-                               target="_blank" rel="noreferrer noopener">тут будет иконка гитхаба</a>
+                            <p className="portfolio__item-description">Стек: React.js, Express.js, MongoDB.</p>
                         </div>
-                        <a className="portfolio__link__external" href="https://tsyalgen.nomoredomains.club/"
+                        <a className="portfolio__link-github" href="https://tsyalgen.nomoredomains.club/"
+                           target="_blank" rel="noreferrer noopener"/>
+                        <a className="portfolio__link-external" href="https://tsyalgen.nomoredomains.club/"
                            target="_blank" rel="noreferrer noopener"/>
                     </div>
                     Movie Explorer
                 </li>
                 <li className="portfolio__item">
-                    <img src={job} alt="my job" className="portfolio__item__image"/>
+                    <img src={job} alt="my job" className="portfolio__item-image"/>
                     <div className="portfolio__links">
-                        <input className="portfolio__menu-toggle" id="menu-toggle"
-                               type="checkbox" checked={isChange} onChange={() => {}}/>
-                        <label className="portfolio__link__info" htmlFor="menu-toggle" onClick={handleToggle}/>
-                        <div className="portfolio__menu-box">
-                            <h3>Movie Explorer</h3>
-                            <h4>ваще тут будет красиво отредаченное описание все дела но пока так</h4>
-                            <p>Дипломный проект Яндекс.Практикума.
+                        <label className="portfolio__link-info"/>
+                        <div className="portfolio__item-info">
+                            <h3 className="portfolio__item-title">Movie Explorer</h3>
+                            <p className="portfolio__item-description">Дипломный проект Яндекс.Практикума.
                                 Представляет собой веб-приложение с функционалом поиска и
                                 сохранения в свой профиль фильмов с базы BeatMovie.</p>
-                            <p>Стек: React.js, Express.js, MongoDB.</p>
-                            <a href="https://github.com/tsyalgen/movies-explorer-frontend"
-                               target="_blank" rel="noreferrer noopener">тут будет иконка гитхаба</a>
+                            <p className="portfolio__item-description">Стек: React.js, Express.js, MongoDB.</p>
                         </div>
-                        <a className="portfolio__link__external" href="https://tsyalgen.nomoredomains.club/"
+                        <a className="portfolio__link-github" href="https://tsyalgen.nomoredomains.club/"
+                           target="_blank" rel="noreferrer noopener"/>
+                        <a className="portfolio__link-external" href="https://tsyalgen.nomoredomains.club/"
                            target="_blank" rel="noreferrer noopener"/>
                     </div>
                     Movie Explorer
